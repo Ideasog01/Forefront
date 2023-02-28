c9226fbf1ac202ea748290f9766e413bfe38cf4a
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2023-02-28 21:13:47 +0000

    Hand Animation V1

---
+++ Forefront/Assets/Scripts/Animation/HandAnimations.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandAnimations : MonoBehaviour
+{
+    [SerializeField]
+    private InputDeviceCharacteristics inputDeviceCharacteristics;
+
+    private Animator _handAnimator;
+
+    private InputDevice _targetDevice;
+
+    private void Start()
+    {
+        _handAnimator = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(!_targetDevice.isValid)
+        {
+            InitialiseHand();
+        }
+        else
+        {
+            UpdateHand();
+        }   
+    }
+
+    private void InitialiseHand()
+    {
+        List<InputDevice> inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(inputDeviceCharacteristics, inputDevices);
+
+        if(inputDevices.Count > 0)
+        {
+            _targetDevice = inputDevices[0];
+
+            Debug.Log("Device found!");
+        }
+    }
+
+    private void UpdateHand()
+    {
+        if(_targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            _handAnimator.SetFloat("triggerValue", triggerValue);
+            Debug.Log(triggerValue);
+        }
+        else
+        {
+            _handAnimator.SetFloat("triggerValue", 0);
+        }
+
+        if (_targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            _handAnimator.SetFloat("gripValue", gripValue);
+            Debug.Log(gripValue);
+        }
+        else
+        {
+            _handAnimator.SetFloat("gripValue", 0);
+        }
+    }
+}
